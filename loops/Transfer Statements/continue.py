1f6ef8a5a21cc0d72c1fe41e77c9211e66261877
gorakh7 <gorakhnaths7777@gmail.com>
2019-08-12 20:29:37 +0530

    odd numbers use by continue loop

---
+++ loops/Transfer Statements/continue.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Author : Gorakhnath Suryawanshi
+
+# print odd no in the range o to 9
+n = int(input("Enter No:"))
+for i in range(n):
+    if i%2==0:
+        continue
+    print(i)
