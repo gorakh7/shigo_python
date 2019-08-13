1e789c5c1e25bcf0ff79a05818d68c14074c0eee
gorakh7 <gorakhnaths7777@gmail.com>
2019-08-13 21:59:05 +0530

    append() function

---
+++ Data types/List data type/append.py
@@ -0,0 +1,12 @@
+list = []
+list.append("a")
+list.append("b")
+list.append("c")
+print(list)
+
+# add all elements to list use for loop
+list1=[]
+for i in range(101):
+    if i%10==0:
+        list1.append(i)
+print(list1)        
