983ef5dbaa8f9ceadc8dbceaa350792460416994
gorakh7 <gorakhnaths7777@gmail.com>
2019-08-14 16:05:43 +0530

    Mathematical operations on the set

---
+++ Data types/Set data type/ex2.py
@@ -0,0 +1,14 @@
+# Mathematical operations on the set
+
+# 1 union() function
+x= {10,20,30,40,50}
+y= {30,40,50,60,70}
+print(x|y)
+print(x.union(y))
+
+x1={10,20,30,40}
+y1={30,40,50,60}
+print(x1&y1)
+print(x1.intersection(y1))
+
+print(x1.difference(y))
